976a10dbafcc3d30231232b53bd21a3115b7c3ba
Sibna-sherin <sibna001@gmail.com>
2024-10-26 20:46:19 +0530

    it is what it is..at this point it looks good but is "useless"

---
+++ Home/Home.js
@@ -0,0 +1,11 @@
+
+    
+        function goToFuture() {
+            alert("Navigating to the future...");
+            // Additional future navigation logic
+        }
+
+        function goToPast() {
+            alert("Navigating to the past...");
+            // Additional past navigation logic
+        }
